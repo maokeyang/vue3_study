cd5bdb3b8ce47d7cbdcf01e08498e878ac09c56a
wanghan <wanghan143@jd.com>
2021-01-06 12:21:06 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,21 +2,10 @@ module.exports = {
   base: '/vue3_study/', /* 基础虚拟路径 */
   dest: 'docs/dist', /* 打包文件基础路径, 在命令所在目录下 */
   title: 'Vue3+TS 快速上手', // 标题
-  description: '尚硅谷前端研究院', // 标题下的描述
+  description: 'Vue3+TS 快速上手', // 标题下的描述
   themeConfig: { // 主题配置
     logo: '/images/logo.png',
     nav: [
-      { text: '官网', link: 'http://www.atguigu.com' },
-      { text: '谷粒学院', link: 'http://www.gulixueyuan.com/' },
-      { 
-        text: '学习路线', 
-        items: [
-          { text: '前端', link: 'http://www.atguigu.com/web/' },
-          { text: 'Java', link: 'http://www.atguigu.com/kecheng.shtml' },
-          { text: '大数据', link: 'http://www.atguigu.com/bigdata/' }
-        ] 
-      },
-      { text: '全套视频资料', link: 'http://www.gulixueyuan.com/' },
     ],
     sidebar: [ // 左侧导航
       '00_课程介绍',
